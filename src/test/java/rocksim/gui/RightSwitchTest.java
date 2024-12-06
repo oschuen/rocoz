ef355c23c5a2cf93c7cd52dfc76e622a3cb0dd82
oschuen <oschuen@users.noreply.github.com>
2024-12-06 20:13:37 +0100

    Deployable

--- src/test/java/rocksim/gui/RightSwitchTest.java
+++ src/test/java/rocksim/gui/RightSwitchTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2023 Oliver Schünemann (oschuen@users.noreply.github.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package rocksim.gui;
 
 import static org.junit.Assert.assertEquals;
@@ -40,10 +55,10 @@ public class RightSwitchTest {
     assertEquals(Optional.empty(), weiche.getFollowUpDirection(Direction.WEST));
     assertEquals(Optional.empty(), weiche.getFollowUpDirection(Direction.SOUTH));
     weiche.setOrientation(Direction.SOUTH);
-    assertEquals(Optional.of(Direction.EAST), weiche.getFollowUpDirection(Direction.NORTH));
-    assertEquals(Optional.of(Direction.SOUTH), weiche.getFollowUpDirection(Direction.WEST));
-    assertEquals(Optional.empty(), weiche.getFollowUpDirection(Direction.EAST));
-    assertEquals(Optional.empty(), weiche.getFollowUpDirection(Direction.SOUTH));
+    assertEquals(Optional.of(Direction.WEST), weiche.getFollowUpDirection(Direction.SOUTH));
+    assertEquals(Optional.of(Direction.NORTH), weiche.getFollowUpDirection(Direction.EAST));
+    assertEquals(Optional.empty(), weiche.getFollowUpDirection(Direction.WEST));
+    assertEquals(Optional.empty(), weiche.getFollowUpDirection(Direction.NORTH));
     weiche.setOrientation(Direction.WEST);
     assertEquals(Optional.of(Direction.NORTH), weiche.getFollowUpDirection(Direction.WEST));
     assertEquals(Optional.of(Direction.EAST), weiche.getFollowUpDirection(Direction.SOUTH));
